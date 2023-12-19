915ece69a944f1461baa4b9d9456051ed0a76fce
vpsbotnew <121183724+vpsbotnew@users.noreply.github.com>
2023-12-19 12:27:58 +0630

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config(object):
       BOT_TOKEN = "5244640547:AAHCDdX2pYDeJ_dHzTp6EJGeOg1X-4PhjJA"
       API_ID = int("20512581")
       API_HASH = "86e23f5f868190ca18c2a542247e51e6"
-      CAPTION_TEXT = "\n‼️Join Now Main Channel\n@YNMOVIEONE\n@STARMOVIEONE\n\n[🩸2024 Update ဇာတ်ကားသစ်သီးသန့်](t.me/YNMOVIETWO)\n\n[🩸ရုပ်ရှင်စုံသောင်းချီရှာဖွေကြည့်နိုင်တဲ့ဂရု](t.me/+qqQ2cCWKwDtmOGVl)\n\n[🩸စီးရီးVIP နှင့် 18+VIP ဝင်ရန်နှိပ်ပါ ](t.me/YNVIPMEMBER)\n\n[🩸မင်မင်ချန်နယ်အားလုံး Join ရန်နှိပ်ပါ](https://t.me/YNMOVIEONE/11953)"
+      CAPTION_TEXT = "\n‼️Join Now Main Channel\n👉@YNMOVIEONE\n👉@STARMOVIEONE\n\n[🩸2024 Update ဇာတ်ကားသစ်သီးသန့်](t.me/YNMOVIETWO)\n\n[🩸ရုပ်ရှင်စုံသောင်းချီရှာဖွေကြည့်နိုင်တဲ့ဂရု](t.me/+qqQ2cCWKwDtmOGVl)\n\n[🩸စီးရီးVIP နှင့် 18+VIP ဝင်ရန်နှိပ်ပါ ](t.me/YNVIPMEMBER)\n\n[🩸မင်မင်ချန်နယ်အားလုံး Join ရန်နှိပ်ပါ](https://t.me/YNMOVIEONE/11953)"
       CAPTION_POSITION = "bottom"
       ADMIN_USERNAME = "YoeNaung"
